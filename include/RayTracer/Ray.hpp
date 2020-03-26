263a271d706d57f33d30a1304bbec2152899c5c0
Xu Wei <xu.wei.31415@gmail.com>
2020-03-26 14:51:07 -0700

    fix epsilon

--- include/RayTracer/Ray.hpp
+++ include/RayTracer/Ray.hpp
@@ -9,4 +9,4 @@ struct Ray {
   glm::vec3 operator()(float t) const { return origin + t * direction; }
 };
 
-static inline const float SHADOW_EPSILON = 1E-4f; // Avoid self-intersection
+static inline const float SHADOW_EPSILON = 1E-3f; // Avoid self-intersection
