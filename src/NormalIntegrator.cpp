65a58978b49351ef430f43e94913f960b75735a9
Xu Wei <xu.wei.31415@gmail.com>
2020-04-01 22:42:52 -0700

    debug NormalIntegrator

--- src/NormalIntegrator.cpp
+++ src/NormalIntegrator.cpp
@@ -5,7 +5,7 @@ glm::vec3 NormalIntegrator::Shade(const Ray &ray) const {
 
   auto color = vec3(0.f);
   if (const auto hit = scene_.Trace(ray)) {
-    color = (hit->normal + 1.f) / 2.f;
+    color = (normalize(hit->normal) + 1.f) / 2.f;
   }
   return color;
 }
